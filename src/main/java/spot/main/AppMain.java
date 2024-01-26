7eb9772ec58e063f28fadb774cc2eedfc691fe65
Diego Goya <124099625+diiegogd@users.noreply.github.com>
2024-01-26 13:44:55 +0000

    Integracion de interfaz
    Co-Authored-By: Flavia <125925345+flawwa@users.noreply.github.com>

--- src/main/java/spot/main/AppMain.java
+++ src/main/java/spot/main/AppMain.java
@@ -2,23 +2,34 @@ package spot.main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class AppMain  extends Application{
+public class AppMain extends Application {
 
 	private MainController mainController;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		mainController = new MainController();	
-		
-		primaryStage.setTitle("StatsMetrics");
-		primaryStage.setScene(new Scene(mainController.getView(), 800, 600));
-		primaryStage.show();
+		mainController = new MainController();
 
-		primaryStage.minWidthProperty().bind(mainController.getView().prefWidthProperty());
-		primaryStage.minHeightProperty().bind(mainController.getView().prefHeightProperty());
-		
+		primaryStage.setTitle("SpotMetrics");
+
+		// Crear una escena con un Pane como nodo raíz
+		Pane rootPane = mainController.getView();
+		Scene scene = new Scene(rootPane);
+
+		// Establecer tamaño fijo para el nodo raíz (en este caso, el Pane)
+		double sceneWidth = 800; // Ancho deseado
+		double sceneHeight = 500; // Alto deseado
+		rootPane.setPrefSize(sceneWidth, sceneHeight);
+
+		primaryStage.setScene(scene);
+
+		// Deshabilitar la capacidad de cambiar el tamaño de la ventana
+		primaryStage.setResizable(false);
+
+		primaryStage.show();
 	}
 
 }
